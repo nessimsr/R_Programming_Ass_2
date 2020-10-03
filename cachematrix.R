4103dfca444fa75fb080020e19320c553ae4e6c1
nessimsr <72280660+nessimsr@users.noreply.github.com>
2020-10-02 19:07:09 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -6,39 +6,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  
-  makeCacheMatrix <- function(sample = matrix()) {
-    invsample <- NULL
-    set <- function(x) {
-      Sample <<- x
-      invsample <<- NULL
-    }
-    get <- function() sample
-    setInverse <- function(inverse) inv <<- inverse
-    getInverse <- function() invsample
-    list(set = set,
-         get = get,
-         setInverse = setInverse,
-         getInverse = getInverse)
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
   }
-  
-  
-  ## Write a short comment describing this function
-  ## This function computes the inverse of the special "matrix" created by 
-  ## makeCacheMatrix above. If the inverse has already been calculated (and the 
-  ## matrix has not changed), then it should retrieve the inverse from the cache.
-  
-  cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    cacheSolve <- function(sample, ...) {
-      ## Return a matrix that is the inverse of 'sample'
-      inv <- sample$getInverse()
-      if (!is.null(inv)) {
-        message("getting cached data")
-        return(invsample)
-      }
-      mat <- sample$get()
-      invsample <- solve(mat, ...)
-      sample$setInverse(invsample)
-      invsample
-    }}}
+  get <- function() x
+  setinverse <- function(i) inverse <- i
+  getinverse <- function() inverse
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached inverse")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
+}
+
+# data <- matrix(c(1, 1, 4, 0, 3, 1, 4, 4, 0), nrow=3, ncol=3)
